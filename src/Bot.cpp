a99a4f27013f2da433647cce74c97a31534eaf10
ExFreY <mmarey0310@gmail.com>
2026-03-11 13:22:14 +0300

    fixed the problem of placing ships near the border

--- src/Bot.cpp
+++ src/Bot.cpp
@@ -2,31 +2,51 @@
 #include <random>
 #include <iostream>
 
-std::optional<Field> StupidBot::randomGenerator() {
-        Field field;
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        bool isSet = false;
-        Ship ship{};
-        std::array<int, 10> ships = {0, 1, 2, 3, 1, 2, 3, 2, 3, 3};
-        int countSettingAttempts = 0;
+std::optional<Field> StupidBot::randomGenerator()
+{
+    Field field;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    Ship ship{};
+    std::array<int, 10> ships = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
+
         for (const int& currentShip : ships) {
-            isSet = false;
+            bool isSet = false;
+            int countSettingAttempts = 0;
+
             while (!isSet) {
-                ship.setSize(4 - currentShip);
-                std::uniform_int_distribution<> distX(1, 7);
-                std::uniform_int_distribution<> distY(1, 7);
+                ship.setSize(currentShip);
+
                 std::uniform_int_distribution<> dist01(0, 1);
-                ship.setX(distX(gen) + currentShip);
-                ship.setY(distY(gen) + currentShip);
-                ship.setRotation(dist01(gen));
+                int rotation = dist01(gen);
+                ship.setRotation(rotation);
+
+                if (rotation == 0) {
+                    std::uniform_int_distribution<> distX(1, 11 - currentShip);
+                    std::uniform_int_distribution<> distY(1, 10);
+
+                    ship.setX(distX(gen));
+                    ship.setY(distY(gen));
+                } else {
+                    std::uniform_int_distribution<> distX(1, 10);
+                    std::uniform_int_distribution<> distY(1, 11 - currentShip);
+
+                    ship.setX(distX(gen));
+                    ship.setY(distY(gen));
+                }
+
                 isSet = field.setShip(ship);
-                if (!isSet) ++countSettingAttempts;
+
+                if (!isSet) {
+                    ++countSettingAttempts;
+                }
+
                 if (countSettingAttempts > 100) {
-                    std::cout << "shit\n";
+                    std::cout << "generation failed\n";
                     return std::nullopt;
                 }
             }
         }
-    return field;
-}
+
+        return field;
+    }
